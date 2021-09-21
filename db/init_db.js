6427bfa0a465d124c37e1acf0d461ce2d6147462
Chris Wong <cw12355@gmail.com>
2021-09-21 15:32:23 -0700

    added more passages to database

--- db/init_db.js
+++ db/init_db.js
@@ -58,6 +58,26 @@ async function populateInitialPassages() {
                 author: 'Pablo Neruda',
                 title: '100 Love Sonnets',
                 excerpt: 'I love you without knowing how, or when, or from where. I love you simply, without problems or pride: I love you in this way because I do not know any other way of loving but this, in which there is no I or you, so intimate that your hand upon my chest is my hand, so intimate that when I fall asleep your eyes close.'
+            },
+            {
+                author: 'Louisa May Alcott',
+                title: 'Little Women',
+                excerpt: "There are many Beths in the world, shy and quiet, sitting in corners till needed, and living for others so cheerfully that no one sees the sacrifices till the little cricket on the hearth stops chirping, and the sweet, sunshiny presence vanishes, leaving silence and shadow behind."
+            },
+            {
+                author: 'C. S. Lewis',
+                title: 'The Lion, The Witch, And The Wardrobe',
+                excerpt: 'Wrong will be right, when Aslan comes in sight, At the sound of his roar, sorrows will be no more, When he bares his teeth, winter meets its death, And when he shakes his mane, we shall have spring again.'
+            },
+            {
+                author: 'Haruki Murakami',
+                title: 'Kafka On The Shore',
+                excerpt: "And once the storm is over you won’t remember how you made it through, how you managed to survive. You won’t even be sure, in fact, whether the storm is really over. But one thing is certain. When you come out of the storm you won’t be the same person who walked in. That’s what this storm’s all about."
+            },
+            {
+                author: 'Ernest Hemingway',
+                title: 'A Farewell To Arms',
+                excerpt: 'In the late summer of that year we lived in a house in a village that looked across the river and the plain to the mountains. In the bed of the river there were pebbles and boulders, dry and white in the sun, and the water was clear and swiftly moving and blue in the channels. Troops went by the house and down the road and the dust they raised powdered the leaves of the trees.'
             }
         ];
 
